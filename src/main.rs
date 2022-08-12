940ca94d0614da4418211398b08614ae5b83db15
Sahana Tankala <sahanatankala@gmail.com>
2022-08-12 15:16:09 -0400

    Add clap for CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,45 @@
-use midilang;
-use std::{env, collections::BinaryHeap};
+use clap::Parser;
+use log::{self, LevelFilter, error};
+use env_logger::{self, Builder, WriteStyle, Target};
+use midilang::parse::MParseError;
+
+/// A Program to compile midi into executable code
+#[derive(Parser, Debug)]
+#[clap(name = "Midi Lang")]
+#[clap(author = "sahanatankala@gmail.com")]
+#[clap(author = "0.1")]
+#[clap(about = "An assembly compiler for MIDI files", long_about = None)]
+struct MidilangCli {
+    
+    #[clap(short = 'm', long = "midi", value_parser, value_name = "FILE")]
+    file_name: Option<String>,
+
+    #[clap(long, value_parser, value_name = "BF_FILE")]
+    brainfuck: Option<String>,
+
+    #[clap(short, long, action)]
+    debug: bool,
+
+    #[clap(short, long, action)]
+    verbose: bool
+}
 
 fn main() {
-    let arguments: Vec<String> = env::args().collect();
+    let cli_args = MidilangCli::parse();
 
-    if arguments.len() < 2 {
-        // UNIMPLEMENTED
-        midilang::run_interactive();
-    } else {
-        midilang::run(&arguments[1]);
+    let log_builder = Builder::new()
+            .filter(None, if cli_args.debug {LevelFilter::Trace} else {LevelFilter::Warn})
+            .write_style(WriteStyle::Auto)
+            .target(if cli_args.verbose {Target::Stdout} else {Target::Stderr} )
+            .init();
+
+    if let Some(bf) = cli_args.brainfuck {
+        midilang::from_brainfuck(&bf);
     }
-    
+    if let Some(path) = cli_args.file_name {
+        match midilang::run(&path) {
+            Err(e) => error!("Application Error {}", e),
+            Ok(x) => println!("Ran successfully!")
+        }
+    }
 }
